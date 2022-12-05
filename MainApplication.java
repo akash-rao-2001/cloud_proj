ef859d22642db019daf9f893a5153b7ba325105a
Akash Rao <rao28271@gmail.com>
2022-12-05 08:55:45 +0530

    Add files via upload

---
+++ MainApplication.java
@@ -0,0 +1,33 @@
+/**
+ *     PureEdgeSim:  A Simulation Framework for Performance Evaluation of Cloud, Edge and Mist Computing Environments 
+ *
+ *     This file is part of PureEdgeSim Project.
+ *
+ *     PureEdgeSim is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     PureEdgeSim is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with PureEdgeSim. If not, see <http://www.gnu.org/licenses/>.
+ *     
+ *     @author Akash Rao
+ **/
+package com.mechalikh.pureedgesim;
+
+import com.mechalikh.pureedgesim.simulationmanager.Simulation;
+
+public class MainApplication {
+
+	public static void main(String[] args) {
+		
+		// Launch a simulation with default settings
+		new Simulation().launchSimulation();
+	}
+
+}
